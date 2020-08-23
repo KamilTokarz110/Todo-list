f0724cb2c55a43f433b4c39607e64c1247709d9f
KamilTokarz110 <56870559+BialaMewa110@users.noreply.github.com>
2020-08-23 18:34:33 +0200

    Done

--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 //Listenery
-
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('input', filterTodo);
 
 //Funkcje
 function addTodo(e) {
@@ -21,6 +23,8 @@ function addTodo(e) {
 
     todoLi.classList.add('todo-item');
     todoDiv.appendChild(todoLi);
+    //Zapisywanie
+    saveLocalTodos(todoInput.value);
     //Przycisk Ukonczenia
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class="fas fa-check"></i>';
@@ -44,8 +48,104 @@ function deleteCheck(e) {
     //Usuwanie todo
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        todo.classList.add('fall');
+        removeLocalTodos(todo);
+        //Animacja
+
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        });
 
     }
 
+    //Check
+    if (item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function getTodos() {
+
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function (todo) {
+        //Todo DIV
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        //LI
+        const todoLi = document.createElement('li');
+        todoLi.innerText = todo;
+
+        todoLi.classList.add('todo-item');
+        todoDiv.appendChild(todoLi);
+
+        //Przycisk Ukonczenia
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>';
+        completedButton.classList.add('complete-btn');
+        todoDiv.appendChild(completedButton);
+        //Przycisk usuniecia
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+        trashButton.classList.add('trash-btn');
+        todoDiv.appendChild(trashButton);
+        //Dodaj do listy
+        todoList.appendChild(todoDiv);
+
+    });
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
